8af136fe59f5247237f9b22c6e1744c71c4b1a0f
Yosua Tarigan <yosuatarigan7@gmail.com>
2021-09-04 09:53:57 +0700

    Updates

---
+++ precache-manifest.2dc2f1dbeb9277df81b4578cf9d0d72c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bae304381522f0b0df7f2ec58fe1a629",
+    "url": "/index.html"
+  },
+  {
+    "revision": "acc8066fa12b8e8adf82",
+    "url": "/static/css/2.ca1c6826.chunk.css"
+  },
+  {
+    "revision": "3086608a8d7db7e28f8c",
+    "url": "/static/css/main.1ae41838.chunk.css"
+  },
+  {
+    "revision": "acc8066fa12b8e8adf82",
+    "url": "/static/js/2.dc3918b0.chunk.js"
+  },
+  {
+    "revision": "88f89f7490135e082f0b84c6d12550cc",
+    "url": "/static/js/2.dc3918b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3086608a8d7db7e28f8c",
+    "url": "/static/js/main.cd54c9c4.chunk.js"
+  },
+  {
+    "revision": "45493b715149f4d23866",
+    "url": "/static/js/runtime-main.1c6855af.js"
+  },
+  {
+    "revision": "3ba1400c1608a913c97d6dd02fca829e",
+    "url": "/static/media/belajar1.3ba1400c.jpeg"
+  },
+  {
+    "revision": "b337f2e9ab24ccfe8f19b96f6e2ab8b8",
+    "url": "/static/media/bg-alumni.b337f2e9.jpg"
+  },
+  {
+    "revision": "526a333a9a04b472ff8330fa9bac37cc",
+    "url": "/static/media/bg-pendaftar.526a333a.jpg"
+  },
+  {
+    "revision": "a79f983aec1ba8bcc68236781f589bbd",
+    "url": "/static/media/fasilitas-min.a79f983a.png"
+  },
+  {
+    "revision": "2519033d6b8653a71d2b2ac13c24793e",
+    "url": "/static/media/logosekolah-min.2519033d.png"
+  },
+  {
+    "revision": "654cd0ebed71b1e7696efaf9f0ba0d6c",
+    "url": "/static/media/teacher-min.654cd0eb.png"
+  },
+  {
+    "revision": "107d665e42c97869d0185b3c7b77466d",
+    "url": "/static/media/testimonials-bg.107d665e.jpg"
+  },
+  {
+    "revision": "8d3b771360314710c53191baa8bb639f",
+    "url": "/static/media/topiwisuda-min.8d3b7713.png"
+  }
+]);
